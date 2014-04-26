02f1ea54a9a7a773eb90ffd28e3ea47c95cb22ec
Arne Roomann-Kurrik <kurrik@gmail.com>
2014-04-25 20:44:21 -0700

    Stubbing out levels.go a bit more.

--- src/level.go
+++ src/level.go
@@ -3,15 +3,23 @@ package main
 import (
 	twodee "../libs/twodee"
 	"github.com/kurrik/tmxgo"
+	"strings"
 )
 
 type Level struct {
+	Grids []*twodee.Grid
 }
 
 func LoadLevel(path string) (l *Level, err error) {
 	var (
-		data []byte
-		m    *tmxgo.Map
+		data  []byte
+		m     *tmxgo.Map
+		layer *tmxgo.Layer
+		i, j  int32
+		grids = []*twodee.Grid{}
+		grid  *twodee.Grid
+		tiles []*tmxgo.Tile
+		tile  *tmxgo.Tile
 	)
 	if data, err = ioutil.ReadFile(path); err != nil {
 		return
@@ -19,4 +27,23 @@ func LoadLevel(path string) (l *Level, err error) {
 	if m, err = tmxgo.ParseMapString(string(data)); err != nil {
 		return
 	}
+	for i, layer = range m.Layers {
+		if !strings.HasPrefix(layer.Name, "layer") {
+			continue
+		}
+		if tiles, err = m.TilesFromLayerIndex(i); err != nil {
+			return
+		}
+		grid = twodee.NewGrid(m.Width, m.Height)
+		for i, t = range tiles {
+			if t != nil {
+				grid.SetIndex(i, true)
+			}
+		}
+		grids = append(grids, grid)
+	}
+	l = &Level{
+		Grids: grids,
+	}
+	return
 }
