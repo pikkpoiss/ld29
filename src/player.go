0a66f6a2992c78d68a303df74330b73d67a668ec
Wes <readybreak@gmail.com>
2014-04-25 23:12:33 -0700

    Initial player struct.

--- src/player.go
+++ src/player.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"../libs/twodee"
+)
+
 type Player struct {
+	*twodee.BaseEntity
+	Health      float64
+	eventSystem *twodee.GameEventHandler
+}
+
+func (p *Player) MoveToListener() {}
+
+func NewPlayer(e *twodee.BaseEntity, eventSystem *twodee.GameEventHandler) (player *Player) {
+	player = &Player{
+		e,
+		100.0,
+		eventSystem,
+	}
+	return
 }
