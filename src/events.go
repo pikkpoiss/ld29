9f216d1c7fb68d1c8b82355f893e062704d40c11
Wes <readybreak@gmail.com>
2014-04-25 23:03:16 -0700

    Add player move event.

--- src/events.go
+++ src/events.go
@@ -9,9 +9,23 @@ const (
 	DownLayer
 	UpWaterLevel
 	DownWaterLevel
+	PlayerMoveEventType
 	sentinel
 )
 
 const (
 	NumGameEventTypes = int(sentinel)
 )
+
+type PlayerMoveEvent struct {
+	*twodee.BasicGameEvent
+	EndPoint *twodee.Point
+}
+
+func NewPlayerMoveEvent(pt *twodee.Point) (e *PlayerMoveEvent) {
+	e = &PlayerMoveEvent{
+		twodee.NewBasicGameEvent(PlayerMoveEventType),
+		EndPoint: pt,
+	}
+	return
+}
