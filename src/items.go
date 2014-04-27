c292ad7d1837005ab6e6b6c9965f179244b5cd98
Wes <readybreak@gmail.com>
2014-04-26 20:11:15 -0700

    Add a 'type' for items, such as LayerThresholdItem and InventoryItem.

--- src/items.go
+++ src/items.go
@@ -5,11 +5,12 @@ import (
 )
 
 type ItemType int
+type ItemId int
 
 // Corresponds with frame indices
 const (
-	_                 = iota
-	ItemDown ItemType = iota
+	_               = iota
+	ItemDown ItemId = iota
 	ItemUp
 	Item1
 	Item2
@@ -19,25 +20,43 @@ const (
 	item_sentinel
 )
 
+// Various item types
+const (
+	LayerThresholdItem ItemType = iota
+	InventoryItem
+)
+
+var ItemIdToType = map[ItemId]ItemType{
+	ItemDown:  LayerThresholdItem,
+	ItemUp:    LayerThresholdItem,
+	Item1:     InventoryItem,
+	Item2:     InventoryItem,
+	Item3:     InventoryItem,
+	Item4:     InventoryItem,
+	ItemFinal: InventoryItem,
+}
+
 const (
 	NumberOfItemTypes = int(item_sentinel)
 )
 
 type Item struct {
 	*twodee.BaseEntity
-	Id   ItemType
+	Id   ItemId
+	Type ItemType
 	Name string
 }
 
-func NewItem(itemType ItemType, name string, x, y, w, h float32) (item *Item) {
+func NewItem(itemId ItemId, itemType ItemType, name string, x, y, w, h float32) (item *Item) {
 	item = &Item{
-		BaseEntity: twodee.NewBaseEntity(x, y, w, h, 0, int(itemType)),
-		Id:         itemType,
+		BaseEntity: twodee.NewBaseEntity(x, y, w, h, 0, int(itemId)),
+		Id:         itemId,
+		Type:       itemType,
 		Name:       name,
 	}
 	return
 }
 
-func (i *Item) getType() (itemType ItemType) {
-	return i.Id
+func (i *Item) getType() ItemType {
+	return i.Type
 }
