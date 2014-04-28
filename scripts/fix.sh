0a481eb5d9bd917df3ddb3dddf57524364d37acb
Arne Roomann-Kurrik <kurrik@gmail.com>
2014-04-27 19:58:29 -0700

    Update for wes' libGLEW

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -4,6 +4,7 @@ chmod +w *.dylib
 
 BINARY=heavydrizzle
 
+install_name_tool -change /usr/local/lib/libGLEW.1.10.0.dylib @executable_path/libGLEW.1.10.0.dylib ${BINARY}
 install_name_tool -change /usr/lib/libGLEW.1.9.0.dylib @executable_path/libGLEW.1.9.0.dylib ${BINARY}
 install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib ${BINARY}
 install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib libSDL-1.2.0.dylib
